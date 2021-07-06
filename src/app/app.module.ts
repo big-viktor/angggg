19d5a27a1742ea4f9edb58f3c3bd983cf4bdbb4f
big-viktor <svitlik.viktor78@gmail.com>
2021-07-06 14:57:00 +0300

    info

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SettingComponent } from '../setting/setting.component';
+import { HeaderComponent } from '../header/header.component';
+import { MenuComponent } from '../menu/menu.component';
+import { SelectComponent } from '../select/select.component';
+import { FooterComponent } from '../footer/footer.component';
+import { PageContactComponent } from '../page-contact/page-contact.component';
+import { ContactComponent } from '../contact/contact.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SettingComponent,
+    HeaderComponent,
+    MenuComponent,
+    SelectComponent,
+    FooterComponent,
+    PageContactComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
