ef3d071a09a1165fc21e76d1af7402e2f91a3e5d
big-viktor <svitlik.viktor78@gmail.com>
2021-07-19 19:35:08 +0300

    commit

--- src/moderation/moderation.component.ts
+++ src/moderation/moderation.component.ts
@@ -3,41 +3,36 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-moderation',
   templateUrl: './moderation.component.html',
-  styleUrls: ['./moderation.component.scss']
+  styleUrls: ['./moderation.component.scss'],
 })
 export class ModerationComponent implements OnInit {
-  selected = "Откритий";
-  selectedTwo = "Закритий";
-  transmission = "AКПП";
-  engine = " 4.4л/407 л.с. / Бензин ";
-  run = "Пробег 74 000 км";
+  selected = 'Откритий';
+  selectedTwo = 'Закрытый';
+  transmission = 'AКПП';
+  engine = ' 4.4л/407 л.с. / Бензин ';
+  run = 'Пробег 74 000 км';
 
-  nameActiv="";
-    
+  nameActiv = '';
+
   isActive = false;
   isStatusDisActive = true;
-  
-  currentClasses={
+
+  currentClasses = {
     verdanaColor: this.isActive,
     navyColor: this.isStatusDisActive,
-}
- 
+  };
 
-  
-  constructor() {
-   }
+  constructor() {}
 
   ngOnInit(): void {
-    if(this.isActive === true){
+    if (this.isActive === true) {
       this.nameActiv = this.selected;
     }
-    if(this.isStatusDisActive === true){
+    if (this.isStatusDisActive === true) {
       this.nameActiv = this.selectedTwo;
-      this.transmission = "--";
-      this.engine = "--/--/-- ";
-      this.run = "--";
+      this.transmission = '--';
+      this.engine = '--/--/-- ';
+      this.run = '--';
     }
   }
- 
-
 }
