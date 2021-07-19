c9d4f0634d9df3af3292bfd8d57b553a87684c94
big-viktor <svitlik.viktor78@gmail.com>
2021-07-20 02:53:53 +0300

    setting

--- src/completed-lots/completed-lots.component.ts
+++ src/completed-lots/completed-lots.component.ts
@@ -3,60 +3,57 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-completed-lots',
   templateUrl: './completed-lots.component.html',
-  styleUrls: ['./completed-lots.component.scss']
+  styleUrls: ['./completed-lots.component.scss'],
 })
 export class CompletedLotsComponent implements OnInit {
-  
-  constructor() { }
+  constructor() {}
 
+  selected = 'Откритий';
+  selectedTwo = 'Закрытый';
+  transmission = 'AКПП';
+  engine = ' 4.4л/407 л.с. / Бензин ';
+  run = 'Пробег 74 000 км';
 
-  selected = "Откритий";
-  selectedTwo = "Закритий";
-  transmission = "AКПП";
-  engine = " 4.4л/407 л.с. / Бензин ";
-  run = "Пробег 74 000 км";
-
-  nameActiv="";
+  nameActiv = '';
   selecteds = '/assets/Group.png';
   selectedTwos = '/assets/Group (1).png';
   image = '/assets/Group (1).png';
   add = true;
-  
-  toggle(){
-    if(this.add){
+
+  toggle() {
+    if (this.add) {
       this.image = this.selecteds;
       this.add = false;
-    }else{
+    } else {
       this.image = this.selectedTwos;
       this.add = true;
     }
- }
+  }
   isActive = false;
   isStatusDisActive = true;
   isNone = true;
   isTop = true;
 
-  currentClasses={
+  currentClasses = {
     verdanaColor: this.isActive,
     navyColor: this.isStatusDisActive,
-}
-  activDisplay ={
-  displayNone:this.isNone,
-}
-activTop={
-  displayTop:this.isTop,
+  };
+  activDisplay = {
+    displayNone: this.isNone,
+  };
+  activTop = {
+    displayTop: this.isTop,
+  };
 
-}
-  
   ngOnInit(): void {
-    if(this.isActive === true){
+    if (this.isActive === true) {
       this.nameActiv = this.selected;
     }
-    if(this.isStatusDisActive === true){
+    if (this.isStatusDisActive === true) {
       this.nameActiv = this.selectedTwo;
-      this.transmission = "--";
-      this.engine = "--/--/-- ";
-      this.run = "--";
+      this.transmission = '--';
+      this.engine = '--/--/-- ';
+      this.run = '--';
     }
   }
 }
